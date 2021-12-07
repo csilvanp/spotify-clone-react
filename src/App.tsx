7565d2e85423c398fc6ef15cc84c41b40b601290
csilvanp <csilvanp@gmail.com>
2021-12-07 09:58:38 -0300

    banner styles finished

--- src/App.tsx
+++ src/App.tsx
@@ -15,6 +15,35 @@ function App() {
         content="Curta músicas sem anúncios, ouça no modo offline e muito mais. Cancele quando quiser."
         buttonType="normal"
         buttonContent="Aproveite 3 meses grátis"
+        backgroundSize="25rem"
+        backgroundPos="85% 50%"
+        footerContent="Somente no plano Individual. Depois, é só R$ 19,90/mês. 
+        Sujeito a Termos e Condições. Disponível apenas para usuários que nunca assinaram o Premium.
+        Oferta válida até 31 de dezembro de 2021."
+      />
+      <Banner
+        backgroundColor="#F46EBE"
+        backgroundImage="https://www-growth.scdn.co/static/home/wrapped-homepage-banner-graphic-tablet-mobile.svg"
+        fontColor="#202f72"
+        subtitle="#RetrospectivaSpotify"
+        title="A Retrospectiva 2021 chegou"
+        content="Mas só está disponível no app Spotify. Baixe agora para descobrir mais."
+        buttonType="store"
+        backgroundSize="1200px"
+        backgroundPos="800px -100%"
+        footerLink="Ouça os destaques de 2021 aqui"
+      />
+      <Banner
+        backgroundColor="rgb(41, 65, 171)"
+        backgroundImage="https://content-tooling.spotifycdn.com/images/b236bf87-bfd5-4bcb-b51a-77881073039c_lie_circles.svg"
+        fontColor="#1ed760"
+        subtitle="Spotify Free"
+        title="Escutar muda tudo"
+        content="Milhões de músicas e podcasts para explorar. E nem precisa de cartão de crédito."
+        buttonType="normal"
+        buttonContent="Baixe o Spotify Free"
+        backgroundSize="1500px"
+        backgroundPos="100% 0%"
       />
     </div>
   )
--- src/components/Banner/index.tsx
+++ src/components/Banner/index.tsx
@@ -2,11 +2,14 @@ import './styles.css';
 type Props = {
   backgroundColor: string;
   backgroundImage: string;
+  backgroundPos?: string;
+  backgroundSize?: string;
   fontColor: string;
   subtitle: string;
   title: string;
   content: string;
   footerContent?: string;
+  footerLink?: string;
   buttonType: string;
   buttonContent?: string;
 }
@@ -18,24 +21,32 @@ export const Banner = (props: Props) => {
       {
         backgroundColor: props.backgroundColor,
         backgroundImage: `url(${props.backgroundImage})`,
-        color: props.fontColor
+        color: props.fontColor,
+        backgroundPosition: props.backgroundPos,
+        backgroundSize: props.backgroundSize
       }}>
-      <span className="banner-subtitle">{props.subtitle}</span>
-      <h3 className="banner-title">{props.title}</h3>
-      <p className="banner-content">{props.content}</p>
+      <div className="banner-content-container">
+        <span className="banner-subtitle">{props.subtitle}</span>
+        <h3 className="banner-title">{props.title}</h3>
+        <p className="banner-content">{props.content}</p>
 
-      {props.buttonType === 'normal' ?
-        <button className="banner-normal-btn">{props.buttonContent}</button>
-        :
-        <>
-          <button className="banner-appstore-btn"></button>
-          <button className="banner-playstore-btn"></button>
-        </>
-      }
+        {props.buttonType === 'normal' ?
+          <button className="banner-normal-btn" style={{ backgroundColor: props.fontColor, color: props.backgroundColor }}>{props.buttonContent}</button>
+          :
+          <div style={{ display: 'flex', alignItems: "center", marginBottom: '5vh' }}>
+            <img className="banner-store-btn" src="https://www-growth.scdn.co/static/badges/svgs/apple/badge-pt.svg" />
+            <img className="banner-store-btn" src="https://www-growth.scdn.co/static/badges/svgs/google/badge-pt.svg" />
+          </div>
+        }
 
-      {props.footerContent &&
-        <span className="banner-footer-content">{props.footerContent}</span>
-      }
+        {props.footerContent &&
+          <span className="banner-footer-content">{props.footerContent}</span>
+        }
+
+        {props.footerLink &&
+          <a className="banner-footer-link" href="https://open.spotify.com/genre/2021-page" style={{ color: props.fontColor }}>{props.footerLink}</a>
+        }
+      </div>
     </div>
   );
 }
