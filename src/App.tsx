7e30acee9dee7e5727ed4028e77fe86a27e5bb69
csilvanp <csilvanp@gmail.com>
2021-12-09 10:32:01 -0300

    desktop previously finished

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import { Header } from './components/Header';
 import { Banner } from './components/Banner';
+import { Footer } from './components/Footer';
 import './App.css'
 
 function App() {
@@ -45,6 +46,8 @@ function App() {
         backgroundSize="1500px"
         backgroundPos="100% 0%"
       />
+
+      <Footer/>
     </div>
   )
 }
